e2eba0c503197923064239798b1ae76633f73183
qjw2bqn <971996481@qq.com>
2018-08-06 14:43:09 +0800

    ttt

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using OSGeo.GDAL;
+using System;
+
+namespace gdal
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Gdal.SetConfigOption("GDAL_FILENAME_IS_UTF8", "NO");
+            //Gdal.AllRegister();
+            //Console.WriteLine("Hello World!");
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>      
+            WebHost.CreateDefaultBuilder(args).UseStartup<Startup>().Build();
+
+        
+    }
+}
